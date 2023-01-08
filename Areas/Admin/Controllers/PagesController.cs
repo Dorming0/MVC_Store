5a28ad82d789dabe900e4ca3cfcfa907c5efa134
Юрий Асмыкjвич <goker5571@gmail.com>
2023-01-08 22:25:42 +1000

    Добавьте файлы проекта.

---
+++ Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,30 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Объявляем список для представления (PagesVM)
+
+            List<PageVM> pagesList;
+            //Инициализация списка (Db)
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+            //Возвращаем список представления
+
+            return View(pagesList);
+        }
+    }
+}
